6674945888ad1517543b07f02957a6be8719ce4c
Shane McDonough <shanemcdman@gmail.com>
2024-09-27 10:25:24 -0700

    add ability to change date quickly

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,5 +1,6 @@
 const createRemoveBookmarkButton = document.getElementById('create-remove-bookmark');
 const resetButton = document.getElementById('reset-button');
+const datePickerOut = document.getElementById('#datepicker-out');
 createRemoveBookmarkButton.classList.remove('hidden');
 
 let bookmarkId = null
@@ -34,13 +35,28 @@ function setUpButton(hasBookmark) {
 	}
 }
 
-hasBookmark().then(setUpButton);
-resetButton.onclick = async () => {
-	let url = new URL(window.location);
-	url.searchParams.set('date', Date.now());
-	url = url.toString();
+function updateBookmark(url) {
 	if(await hasBookmark()) {
 		await chrome.runtime.sendMessage({ type: 'update', id: bookmarkId, url });
 	}
-	window.location = url;
 }
+
+function resetCount(url, date = null){
+    if(date === null) date = Date.now();
+    url.searchParams.set('date', date);
+    window.history.pushState({ path: url.toString() }, '', url.toString());
+    datePickerOut.value = convertISODate(date);
+}
+
+hasBookmark().then(setUpButton);
+resetButton.onclick = async () => {
+	let url = new URL(window.location);
+	resetCount(url);
+	updateBookmark(url);
+}
+datePickerOut.removeEventListener('change');
+datePickerOut.addEventListener('change', () => {
+	let url = new URL(window.location);
+	resetCount(url, new Date(els.datePickerOut.value) - 0);
+	updateBookmark(url);
+});
