a28f9e32191a50d52a92a6c3b1a08c53e9fa3b30
Shane Walker <shanemcdman@gmail.com>
2026-07-22 19:18:08 -0700

    change months to 30 days

--- extension/shared.js
+++ extension/shared.js
@@ -3,7 +3,7 @@ const MINUTE = 60  * SECOND;
 const HOUR   = 60  * MINUTE;
 const DAY    = 24  * HOUR;
 const WEEK   = 7   * DAY;
-const MONTH  = 31  * DAY;
+const MONTH  = 30  * DAY;
 const YEAR   = 365.25 * DAY;
 
 const UNITS = Object.freeze({
